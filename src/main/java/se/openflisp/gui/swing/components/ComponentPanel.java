70b44996eac1b40fa2407249c57e0e0a346299be
Oskar Selberg <Stekpanna@dhcp-202083.eduroam.chalmers.se>
2014-02-26 14:36:50 +0100

    added more gates to the ComponentPanel and fixed an aerror inside GateView

--- src/main/java/se/openflisp/gui/swing/components/ComponentPanel.java
+++ src/main/java/se/openflisp/gui/swing/components/ComponentPanel.java
@@ -17,36 +17,22 @@
 package se.openflisp.gui.swing.components;
 
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DragGestureEvent;
 import java.awt.dnd.DragGestureListener;
-import java.awt.dnd.DragSource;
-import java.io.IOException;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.ListCellRenderer;
-import javax.swing.SwingUtilities;
-import javax.swing.TransferHandler;
 
-
-import se.openflisp.sls.Component;
+import se.openflisp.sls.Signal;
 import se.openflisp.sls.component.AndGate;
+import se.openflisp.sls.component.ConstantGate;
+import se.openflisp.sls.component.NandGate;
 import se.openflisp.sls.component.NotGate;
+import se.openflisp.sls.component.OrGate;
 
 /**	
  * Component for showing all components and enabling drag and drop creation.
@@ -56,28 +42,84 @@ import se.openflisp.sls.component.NotGate;
  */
 @SuppressWarnings("serial")
 public class ComponentPanel extends JPanel {
+	//JPanels for the panel
+	private JPanel standardGates;
+	private JPanel colOne;
+	private JPanel colTwo;
+	
+	
+	//Layout for the panel
+	private GridBagLayout layout;
+	private GridBagConstraints constraints;
 	
 	// All components to show
-	//TODO Add more
-	ComponentView andGate;
-	ComponentView notGate;
+	private ComponentView andGate;
+	private ComponentView notGate;
+	private ComponentView constantGate;
+	private ComponentView nandGate;
+	private ComponentView orGate;
+	//private ComponentView norGate;	//TODO add when finished
+	//private ComponentView xorGate;	//TODO add when finiched
 	
 	/**
 	 * Creates the component panel
 	 */
 	public ComponentPanel() {
-		setLayout(new GridBagLayout());
+		constraints = new GridBagConstraints();
+        layout = new GridBagLayout();
+        
+        constraints.weighty = 1.0;	//makes space between the components
+        
+		layout.setConstraints(this, constraints);
+		
+		this.setLayout(layout);
+
+		
 		
 		andGate = new GateView(new AndGate("AndGate"));
 		notGate = new GateView(new NotGate("NotGate"));
+		constantGate = new GateView(new ConstantGate("ConstantGate", Signal.State.LOW));
+		nandGate = new GateView(new NandGate("NandGate"));
+		orGate = new GateView(new OrGate("OrGate"));
+		//norGate = new GateView(new NorGate("NorGate"));	//TODO add when finished
+		//xorGate = new GateView(new XorGate("XorGate"));	//TODO add when finished
+		
 		andGate.setMaximumSize(new Dimension(ComponentView.componentSize,2));
 		notGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));
+		constantGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));
+		nandGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));
+		orGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));
+		//norGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));	//TODO add when finished
+		//xorGate.setMaximumSize(new Dimension(ComponentView.componentSize,ComponentView.componentSize/2));	//TODO add when finished
 		
-		add( Box.createVerticalGlue() );
-		add(andGate);
-		add( Box.createVerticalGlue() );
-		add(notGate);
-		add( Box.createVerticalGlue() );
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		this.add( constantGate, constraints );
+		
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		this.add( notGate, constraints );
+
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		this.add( andGate, constraints );
+	
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+		this.add( orGate, constraints );
+
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		this.add( nandGate, constraints );
+
+		constraints.gridx = 1;
+		constraints.gridy = 1;
+		//this.add( norGate, constraints );	//TODO add when finished
+
+		constraints.gridx = 1;
+		constraints.gridy = 2;
+		//this.add(xorGate, constraints );	//TODO add when finished
+
 		/**
 		 * Enable drag and drop listener, by sending a string to the reciever
 		 */
@@ -91,10 +133,23 @@ public class ComponentPanel extends JPanel {
 						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("AndGate"));
 					else if (view.component instanceof NotGate)
 						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("NotGate"));
-				} catch(Exception e) {
-					e.printStackTrace();
+					else if (view.component instanceof ConstantGate)
+						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("ConstantGate"));
+					else if (view.component instanceof NandGate)
+						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("NandGate"));
+					else if (view.component instanceof OrGate)
+						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("OrGate"));
+					/*
+					else if (view.component instanceof NorGate)
+						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("NorGate"));
+					else if (view.component instanceof XorGate)
+						event.startDrag(ComponentPanel.this.notGate.ds.DefaultMoveDrop, new StringSelection("XorGate"));
+					*/
+				} catch(Exception e) {	//TODO CHECK EXEPTION
+					e.printStackTrace();	
 				}
 			}
 		});
 	}
 }
+
